449bec2edff1d0829d735cedc80ed37d40c51412
Radek Necas <radnecx@gmail.com>
2021-11-06 19:45:02 +0100

    Add Guard static class

---
+++ src/eShopWebApi.Core/Tools/Guard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace eShopWebApi.Core.Tools
+{
+    public static class Guard
+    {
+        public static void IsNotNull(object argumentValue, string argumentName)
+        {
+            if (argumentValue == null)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+        }
+
+        public static T ReturnIfChecked<T>(T argumentValue, string argumentName)
+        {
+            IsNotNull(argumentValue, argumentName);
+            return argumentValue;
+        }
+    }
+}
