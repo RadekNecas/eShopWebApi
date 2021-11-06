774dd46c884bb4f1cc881ec025dd21958940bd86
Radek Necas <radnecx@gmail.com>
2021-11-06 21:36:24 +0100

    Add docker compose support for SQL server and initialize database on startup

--- src/eShopWebApi/Startup.cs
+++ src/eShopWebApi/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Linq;
 
 namespace eShopWebApi
 {
@@ -19,11 +21,13 @@ namespace eShopWebApi
 
         public IConfiguration Configuration { get; }
 
+        private bool ShouldUseInMemoryDb => Configuration.GetValue<bool>("eShopWebApi:UseInMemoryApplicationDb");
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var useInMemoryDb = Configuration.GetValue<bool>("eShopWebApi:UseInMemoryApplicationDb");
-            if (useInMemoryDb)
+            if (ShouldUseInMemoryDb)
             {
                 services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("eShopWebApiDatabase"));
             }
@@ -40,10 +44,11 @@ namespace eShopWebApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger, ApplicationDbContext dbContext)
         {
             if (env.IsDevelopment())
             {
+                logger.LogInformation("Developer exception page will be used.");
                 app.UseDeveloperExceptionPage();
             }
 
@@ -57,6 +62,26 @@ namespace eShopWebApi
             {
                 endpoints.MapControllers();
             });
+
+            ApplyMigrationsIfNeeded(dbContext, logger);
+        }
+
+
+        private void ApplyMigrationsIfNeeded(ApplicationDbContext context, ILogger<Startup> logger)
+        {
+            // Migration didn't work for in-memory database so this setting has to be disabled
+            var applyMigrationIfNeeded = Configuration.GetValue<bool>("eShopWebApi:ApplyDbMigrationIfNeeded");
+            if (!ShouldUseInMemoryDb && applyMigrationIfNeeded && context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+                logger.LogInformation("Database was migrated to the latest version. ShouldUseInMemoryDb={shouldUseInMemoryDb}, ApplyMigrationIfNeeded={applyMigrationIfNeeded}",
+                    ShouldUseInMemoryDb, applyMigrationIfNeeded);
+            }
+            else
+            {
+                logger.LogInformation("Database migration was skipped. ShouldUseInMemoryDb={shouldUseInMemoryDb}, ApplyMigrationIfNeeded={applyMigrationIfNeeded}",
+                    ShouldUseInMemoryDb, applyMigrationIfNeeded);
+            }
         }
     }
 }
