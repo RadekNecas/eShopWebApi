8bc56f95721a8fa801031affbb968dc3a6adab07
Radek Necas <radnecx@gmail.com>
2021-11-06 19:44:25 +0100

    Cleanup unused classes

--- src/eShopWebApi.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace eShopWebApi.Core
-{
-    public class Class1
-    {
-    }
-}
--- src/eShopWebApi.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace eShopWebApi.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
